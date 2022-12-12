d7dd27328b7206a07bcb435451ea80d092d429b4
teokri303 <74730383+teokri303@users.noreply.github.com>
2022-12-12 20:00:23 +0200

    task 2 done

--- k_means2.py
+++ k_means2.py
@@ -9,15 +9,20 @@ import plotly.express as exp
 data = pd.read_csv('BX-Users.csv')
 ratings = pd.read_csv('BX-Book-Ratings.csv')
 
+# Katharismos twn ratings apo touw xristes pou den ufistantai sto user dataset kai den tha uparksoun oute sta cluster
+
+# Katharismos pairnontas tin tomi twn User uids kai Rating uids
+uids = list(set(ratings.uid).intersection(set(data.uid)))
+ratings = ratings[ratings.uid.isin(uids)]
+
+# Second option katharismos me merge
+#ratings = pd.merge(ratings, data, how='inner', on='uid')
 
 #---katharismos tou column location gia na meinei mono h xwra---
-for x in range(len(data)):
-    my_str = data.loc[x,"location"]
-    new_val= my_str[my_str.rfind(','):]
-    data.loc[x,"location"] = new_val[1:len(new_val)]
-    # clear people above 110 age
-    if data.loc[x,"age"] >110 or data.loc[x,"age"] <10:
-        data.drop(x,axis=0,inplace=True)
+data.location = data.location.apply(lambda x: x.split(', ')[-1])
+
+# clear people above 110 age
+data = data.query("age >= 10 and age <= 110")
 
 #---katharismos twn countries kathws uphrxan mesa mi uparktes xwres.---
 unique_countries = data['location'].unique()
@@ -25,38 +30,12 @@ unique_countries = data['location'].unique()
 print("Xwres prin to katharisma: ", len(unique_countries))
 
 #---diagrafi twn xwrwn pou emfanizontai ligoteres apo 15 fores kathws opws eidame den einai egkyres
-for country in unique_countries:  
-    if len(data[data['location'] == country]) < 15 :
-        #print(country, "with entries ", len(data[data['location'] == country]), "DROPPED")
-        data.drop(data.loc[data['location'] == country].index, inplace = True) 
+location_freq = data.location.value_counts()
+invalid_countries = location_freq[location_freq < 15].index
+data = data[~data.location.isin(invalid_countries)]
 
 unique_countries = data['location'].unique()
 print("Xwres meta to katharisma: ", len(unique_countries))
-
-#function gia na plottarei tis meses hlikies kathe xwras
-def makePlot(number): 
-
-    for x in range(number):
-        country = unique_countries[x]
-        age18_25 = data[data['location'] == country]['age'][(data[data['location'] == country]['age'] <= 25) & (data[data['location'] == country]['age'] >= 18)]
-        age26_35 = data[data['location'] == country]['age'][(data[data['location'] == country]['age'] <= 35) & (data[data['location'] == country]['age'] >= 26)]
-        age36_45 = data[data['location'] == country]['age'][(data[data['location'] == country]['age'] <= 45) & (data[data['location'] == country]['age'] >= 36)]
-        age46_55 = data[data['location'] == country]['age'][(data[data['location'] == country]['age'] <= 55) & (data[data['location'] == country]['age'] >= 46)]
-        age55above = data[data['location'] == country]['age'][data[data['location'] == country]['age'] >= 56]
-
-        x = ["18-25","26-35","36-45","46-55","55+"]
-        y = [len(age18_25.values),len(age26_35.values),len(age36_45.values),len(age46_55.values),len(age55above.values)]
-
-        plt.figure(figsize=(15,6))
-        sns.barplot(x=x, y=y, palette="rocket")
-        plt.title(country)
-        plt.xlabel("Age")
-        plt.ylabel("COUNT OF PEOPLE")
-        plt.show()  
-
-#makePlot(5)
-
-
 
 #vazoume gia kathe xwra ena monadiko arithmo gia na perasei sto model
 for country in unique_countries:
@@ -66,17 +45,12 @@ for country in unique_countries:
 #dropparoume tis Nan times gia na mporei na mpei sto kmean model
 data = data.dropna()
 
-
+# proetoimasia montelou
 X = np.array(data.loc[:,['location', 'age']])
-
-
 kmeans = KMeans(init="random",n_clusters=3,n_init=10,max_iter=300,random_state=42)
 
-model = kmeans.fit_predict(X)
-
 # to model einai ta labels gia to pou anikei o kathe user
-#print(len(model))
-
+model = kmeans.fit_predict(X)
 
 plt.scatter(X[:,0], X[:,1])
 plt.scatter(kmeans.cluster_centers_[:, 0], 
@@ -97,84 +71,103 @@ data = data.reset_index(drop=True)
 data['cluster'] = model
 
 # edw ksexwrizoyme tous users me vasi to cluster pou tous anatehike
-# isws xreiastei reset index
 clust_1 = data[data['cluster'] == 0]
 clust_2 = data[data['cluster'] == 1]
 clust_3 = data[data['cluster'] == 2]
 
-#print(data.head)
+print('Dimiourgithika 3 cluster xrhstwn me plithos: ')
+print('Cluster 1: ', len(clust_1))
+print('Cluster 2: ', len(clust_2))
+print('Cluster 3: ', len(clust_3))
 
+
+#---------------------RATINGS PART-----------------------------------------
+
+# Apo ola ta ratings einai auta poy einai midenika
 zero_ratings = ratings[ratings['rating'] == 0]
 zero_ratings = zero_ratings.reset_index(drop=True)
-#zero_ratings.head
 
-
+# Apo ola ta ratings einai ta non zero 
 non_zero_ratings = ratings[ratings['rating'] != 0]
 non_zero_ratings = non_zero_ratings.reset_index(drop=True)
 
+print('\n------RATINGS------- ')
+print('All ratings: ' ,len(ratings))
+print('Non zero ratings: ', len(non_zero_ratings))
+print('Zero ratings: ' , len(zero_ratings))
 
-clust_1_users_ratings = pd.DataFrame()
-clust_2_users_ratings = pd.DataFrame()
-clust_3_users_ratings = pd.DataFrame()
+# Einai oi non_zero kritikes twn users tou kathe cluster apo tis opoies tha bgoun oi MO
+clust_1_users_ratings = pd.merge(clust_1, non_zero_ratings, how='inner', on='uid')
+clust_2_users_ratings = pd.merge(clust_2, non_zero_ratings, how='inner', on='uid')
+clust_3_users_ratings = pd.merge(clust_3, non_zero_ratings, how='inner', on='uid')
 
-#Kathe dataframe exei tous users tou kathe cluster kai kathe vivlio pou exoun vathmologisei
-for x in range(len(non_zero_ratings)):
-    if non_zero_ratings.iloc[x]['uid'] in clust_1['uid'].values:
-        clust_1_users_ratings = pd.concat([clust_1_users_ratings, non_zero_ratings.iloc[[x]]])
-
-for x in range(len(non_zero_ratings)):
-    if non_zero_ratings.iloc[x]['uid'] in clust_2['uid'].values:
-        clust_2_users_ratings = pd.concat([clust_2_users_ratings, non_zero_ratings.iloc[[x]]])
-
-for x in range(len(non_zero_ratings)):
-    if non_zero_ratings.iloc[x]['uid'] in clust_3['uid'].values:
-        clust_3_users_ratings = pd.concat([clust_3_users_ratings, non_zero_ratings.iloc[[x]]])
+#clust_1_users_ratings.head()
 
 
-
-print(clust_1_users_ratings.head)
-#print(clust_2_users_ratings.head)
-#print(clust_3_users_ratings.head)
-
-
+# edw tha einai o MO vathmoligion tou kahte vivliou gia to kathe cluster pou tha upologistoun apo katw
 books_rat_clust1 = np.empty((0, 2), int)
 books_rat_clust2 = np.empty((0, 2), int)
 books_rat_clust3 = np.empty((0, 2), int)
 
 
-
+# Ypologismos MO kathe vivliou sto kathe cluster kai eisagwgh se antistoixo pinaka me touw mesous orous
 for book in range(len(zero_ratings)):
     
     # etsi pairno me tin seira isbn apo ta zero
     x = zero_ratings.iloc[book]['isbn']
-
-    # elegxei an yparxei to isbn sta non zero
-    if x in clust_1_users_ratings['isbn'].values:
-        # poses fores uparxei auto to isbn sta non zero
-        #clust_1_users_ratings['isbn'].value_counts()[x]
-        
+    
+    # elegxei an yparxei to isbn sta non zero 
+    if x in clust_1_users_ratings['isbn'].values and (x in books_rat_clust1) == False : 
         # edw vazo se ena neo dataframe oles tis vathmologies twn allwn users gia to biblio
         ongoing_book_rating=clust_1_users_ratings[clust_1_users_ratings['isbn'] == x]
         # vrisko tin mesi timi giauto to vivlio gia tous user pou to exoun vathmologisei
         mean=ongoing_book_rating['rating'].mean()
-        # vazoume sto array to isbn kai dipla ton meso oro twn vathmologiwn
+        mean = round(mean)
+        # vazoume sto array to isbn kai dipla ton meso oro twn vathmologiwn tou cluster
         books_rat_clust1=np.append(books_rat_clust1, np.array([[x, mean]]), axis=0)
 
-    if x in clust_2_users_ratings['isbn'].values:
-        
-        # edw vazo se ena neo dataframe oles tis vathmologies twn allwn users gia to biblio
+    # antistoixa gia to deutero cluster
+    if x in clust_2_users_ratings['isbn'].values and (x in books_rat_clust2) == False: 
         ongoing_book_rating=clust_2_users_ratings[clust_2_users_ratings['isbn'] == x]
-        # vrisko tin mesi timi giauto to vivlio gia tous user pou to exoun vathmologisei
         mean=ongoing_book_rating['rating'].mean()
-        # vazoume sto array to isbn kai dipla ton meso oro twn vathmologiwn
+        mean = round(mean)
         books_rat_clust2=np.append(books_rat_clust2, np.array([[x, mean]]), axis=0)
 
-    if x in clust_3_users_ratings['isbn'].values:
-      
-        # edw vazo se ena neo dataframe oles tis vathmologies twn allwn users gia to biblio
+    # antistoixa gia to trito cluster
+    if x in clust_3_users_ratings['isbn'].values and (x in books_rat_clust3) == False:
         ongoing_book_rating=clust_3_users_ratings[clust_3_users_ratings['isbn'] == x]
-        # vrisko tin mesi timi giauto to vivlio gia tous user pou to exoun vathmologisei
         mean=ongoing_book_rating['rating'].mean()
-        # vazoume sto array to isbn kai dipla ton meso oro twn vathmologiwn
+        mean = round(mean)
         books_rat_clust3=np.append(books_rat_clust3, np.array([[x, mean]]), axis=0)        
 
+
+# pairno mia mia tis midenikes kritikes
+for x in range(len(zero_ratings)):
+    user = zero_ratings.iloc[x]['uid']
+    isbn = zero_ratings.iloc[x]['isbn']
+
+    # elegxos an o user anikei sto cluster1 kai an to vivlio exei vathmologithei apo allous xristes tou cluster 1
+    if user in clust_1['uid'].values and isbn in books_rat_clust1 :
+        book_potition = np.where(books_rat_clust1 == isbn)
+        index = book_potition[0][0]
+        zero_ratings.at[x, 'rating'] = books_rat_clust1[index][1]
+
+    # antistoixa gia ta alla cluster
+    elif user in clust_2['uid'].values and isbn in books_rat_clust2 :
+        book_potition = np.where(books_rat_clust2 == isbn)
+        index = book_potition[0][0]
+        zero_ratings.at[x, 'rating'] = books_rat_clust2[index][1]
+
+    elif user in clust_3['uid'].values and isbn in books_rat_clust3 :
+        book_potition = np.where(books_rat_clust3 == isbn)
+        index = book_potition[0][0]
+        zero_ratings.at[x, 'rating'] = books_rat_clust3[index][1]
+
+        
+# epanenosi me to non zero ratings gia na ta apothikeusoyme wste na xrisimopoiithoun
+updated_ratings = pd.concat([non_zero_ratings, zero_ratings])
+updated_ratings = updated_ratings.reset_index(drop=True)
+
+# apothikeusi sto working directory
+updated_ratings.to_csv('Updated_Ratings.csv')
+print("New ratings is saved. ")
